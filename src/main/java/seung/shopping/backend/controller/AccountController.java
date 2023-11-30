d08e9b3e43c0b37413dd325852b0ea50fe18fecc
admin <tmdco12@naver.com>
2023-11-30 16:49:58 +0900

    controller - jwt

--- src/main/java/seung/shopping/backend/controller/AccountController.java
+++ src/main/java/seung/shopping/backend/controller/AccountController.java
@@ -1,13 +1,18 @@
 package seung.shopping.backend.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import seung.shopping.backend.entity.Member;
 import seung.shopping.backend.repository.MemberRepository;
+import seung.shopping.backend.service.JwtService;
+import seung.shopping.backend.service.JwtServiceImpl;
 
 import java.util.Map;
 
@@ -19,11 +24,24 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+        HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
+        // 멤버값이 널이 아닐경우
         if(member != null) {
-            return member.getId();
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            // id 토큰화
+            String token = jwtService.getToken("id", id);
+            // 토큰을 쿠키에 추가
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+            // 쿠키를 응답값에 준다
+            return ResponseEntity.ok().build();
         }
 
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
