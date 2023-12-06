8bbe0c45542ffcfac2666b55b1e0c3382d7075a1
admin <tmdco12@naver.com>
2023-12-06 20:53:16 +0900

    service - 로직 보완

--- src/main/java/seung/shopping/backend/service/JwtService.java
+++ src/main/java/seung/shopping/backend/service/JwtService.java
@@ -1,5 +1,10 @@
 package seung.shopping.backend.service;
 
+import io.jsonwebtoken.Claims;
+
 public interface JwtService {
+
     String getToken(String key,Object value);
+
+    Claims getClaims(String token);
 }
